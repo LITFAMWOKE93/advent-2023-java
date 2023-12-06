94b9c5e473c0edba97a31de50e8bea0bf1507580
Sam Trafton <samatrafton@gmail.com>
2023-12-06 13:55:43 +0200

    Day 1 part 2

--- Day1Trebuchet.java
+++ Day1Trebuchet.java
@@ -1,18 +1,22 @@
 // String manipulation
 // Need to strip away everything but the first and last digit in a string.
 // Need to read the text file puzzle
+//Part 2: Parse out the the written "digits" as valid numbers
 
 // LESSON LEARNED: Carefully read prompt requirements, it's very easy to start coding a solution for the wrong problem.
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Day1Trebuchet {
     // highly coupled, but I'm still learning
 
-    public static int readFile(int sum, String filename){
+    public static int readFileDigits(int sum, String filename){
         try {
             File puzzleText = new File(filename);
             Scanner scnr = new Scanner(puzzleText);
@@ -86,13 +90,26 @@ public class Day1Trebuchet {
     }
 
 
-    public static void main(String []args) {
-        int sum = 0;
-        int num = readFile(sum, "day_1_trebuchet.txt");
 
-        System.out.println("The trebuchet calibration sum is: " + num);
-
-
-    }
+   
+/* Part 2 defeated me, thanks to u/TiltSword for the answer
+ * I need to do some regex studying, this was pretty clever swapping out
+ * the numeral digits to contain a digit to allow for stripping out the 
+ * numerics and then youre left with all digits in the correct order,
+ * after that you place the first index with the second subtracting the ASCII value of the char type
+ */
+    public static void main(String[] args){
+    long sum=0;
+    try{
+        BufferedReader br=new BufferedReader(new FileReader("day_1_trebuchet.txt"));
+        sum=br.lines().map(s->s.replaceAll("one","o1ne").replaceAll("two","t2wo")
+                        .replaceAll("three","t3hree").replaceAll("four","f4our").replaceAll("five","f5ive")
+                        .replaceAll("six","s6ix").replaceAll("seven","s7even").replaceAll("eight","e8ight")
+                        .replaceAll("nine","n9ine").replaceAll("[a-z]",""))
+                .mapToInt(s->(s.charAt(0)-'0')*10+s.charAt(s.length()-1)-'0').sum();
+        br.close();
+    }catch(Exception e){System.out.println(e.toString());}
+    System.out.println(sum);
+}
     
 }
